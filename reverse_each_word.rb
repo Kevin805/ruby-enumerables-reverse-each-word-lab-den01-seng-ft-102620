5593fcaae22feacde021b033b461ddcc3130ab12
funny-module-5140 <kevin.michael805@gmail.com>
2020-10-19 01:04:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
 new_sentence = []
- sentence.split.each do |backward_words|
+ sentence.collect.each do |backward_words|
  new_sentence << backward_words.reverse
  end
 new_sentence.join(" ")
